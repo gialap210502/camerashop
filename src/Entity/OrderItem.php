41d454e63e20eaf0805c3f0391a29f72cfb8a3ce
gialap210502 <89369650+gialap210502@users.noreply.github.com>
2022-03-19 22:25:55 +0700

    AVOIDING ADDING DUPLICATE ORDERITEM
    Currently, duplicate OrderItem entities (with the same Product) can be added to the Order entity. Let's get things fixed.
    Add an equals() function to the OrderItem class to determine whether the item corresponds to an argument.

--- src/Entity/OrderItem.php
+++ src/Entity/OrderItem.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\OrderItemRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: OrderItemRepository::class)]
 class OrderItem
@@ -64,4 +65,25 @@ class OrderItem
 
         return $this;
     }
+    /**
+     * Tests if the given item given corresponds to the same order item.
+     * @param OrderItem $item
+     * @return bool
+     */
+    public function equals(OrderItem $item): bool
+    {
+        return $this->getProduct()->getId() === $item->getProduct()->getId();
+    }
+
+    /**
+     * Calculates the item total.
+     * @return float|int
+     */
+    public function getTotal(): float
+    {
+        return $this->getProduct()->getPrice() * $this->getQuantity();
+    }
+
+    
+
 }
